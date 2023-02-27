9f5ab59f56f6d0b1522ced593e55c37877f074b9
Mikhail <mikhail86@MyLin.myguest.virtualbox.org>
2023-02-27 17:01:32 +0400

    Add API structure and api paths.

--- django_api/movies_admin/config/urls.py
+++ django_api/movies_admin/config/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('movies.api.urls')),
 ]
 
 if settings.DEBUG:
---
+++ django_api/movies_admin/movies/api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('movies', views.MoviesListApi.as_view()),
+]
