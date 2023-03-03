d0050264747ace15dbecc7aaf9c87279c2927579
Mikhail <mikhail86@MyLin.myguest.virtualbox.org>
2023-03-03 16:00:21 +0400

    Change again (still doesn't work).

--- docker_compose/simple_project/app/run_uwsgi.sh
+++ docker_compose/simple_project/app/run_uwsgi.sh
@@ -4,6 +4,20 @@ set -e
 
 chown djus:djus /var/log
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 uwsgi --strict --ini /app/uwsgi.ini
+
+exec "$@"
+
